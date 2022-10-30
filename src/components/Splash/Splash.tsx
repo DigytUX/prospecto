44f15c46961ce07ac8f370be50150845d3ab5195
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-10-30 17:41:55 -0400

    Added splash

--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import {
+  Container,
   Grid,
   Box,
   Typography,
@@ -19,31 +21,67 @@ export default function Splash({
   image
 }: AppProps){
 
+  const styles = {
+    Splash:{
+      width:'100vw',
+      height:'100vh',
+      display:'flex',
+      alignItems:'center'
+    },
+    LeftContent:{
+      height:'100%',
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    RightContent:{
+      height:'100%',
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    FormWrapper:{
+      display:'flex',
+      height:'auto'
+    },
+    TextField:{
+      width:'100%'
+    },
+    Image:{
+      width:'80%'
+    }
+  }
+
   return (
-    <Grid container>
-      <Grid item xs={12} lg={6}>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <Typography>{title}</Typography>
+    <Box sx={styles.Splash}>
+      <Container maxWidth="xl">
+        <Grid container>
+          <Grid sx={styles.LeftContent} item xs={12} lg={6}>
+            <Box sx={styles.FormWrapper}> 
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Typography variant="h2" component="h1">{title}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="body1" component="p">{text}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField sx={styles.TextField} placeholder="email" type="email" />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField sx={styles.TextField} placeholder="password" type="password" />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button variant="contained">Login</Button>
+                </Grid>
+              </Grid>
+            </Box>
           </Grid>
-          <Grid item xs={12}>
-            <Typography>{text}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField placeholder="email" type="email" />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField placeholder="password" type="password" />
-          </Grid>
-          <Grid item xs={12}>
-            <Button>Login</Button>
+          <Grid sx={styles.RightContent} item xs={12} lg={6}>
+              <Box sx={styles.Image} component="img" src={image} />
           </Grid>
         </Grid>
-      </Grid>
-      <Grid item xs={12} lg={6}>
-          <Box component="img" src={image} />
-      </Grid>
-    </Grid>
+      </Container>
+    </Box>
   )
-
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Box} from '@mui/material'
 import Splash from '../components/Splash/Splash'
+import SalesExecutiveImage from '../assets/sales-executive.svg'
 
 export default function Welcome(){
   return (
   <Splash 
     title="Find businesses that need your services" 
     text="" 
-    image="test" 
+    image={SalesExecutiveImage} 
   />
   )
 }
