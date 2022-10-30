2423f740506eb24684a7ac4124a614df2489c763
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-10-30 16:29:58 -0400

    Added configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDING_ID,
+  FIREBASE_API_ID,
+  FIREBASE_MEASUREMENT_ID
+} from './config/index'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey:FIREBASE_API_KEY,
+  authDomain:FIREBASE_AUTH_DOMAIN,
+  projectId:FIREBASE_PROJECT_ID,
+  storageBucket:FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:FIREBASE_MESSAGING_SENDING_ID,
+  appId:FIREBASE_API_ID,
+  measurementId:FIREBASE_MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
