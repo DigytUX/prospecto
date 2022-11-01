766f0683d4266063e3fd3e293a4fc4dca0769ea4
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-11-01 17:42:36 -0400

    removed unneed lines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,6 @@ import React, {useState, useEffect} from 'react'
 import {Box} from '@mui/material'
 import Splash from '../components/Splash/Splash'
 import SalesExecutiveImage from '../assets/sales-executive.svg'
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import firebase from 'firebase/compat/app'
-
-console.log('firebase', firebase)
 
 /* Public Page  (no auth) */
 export default function Welcome(){
