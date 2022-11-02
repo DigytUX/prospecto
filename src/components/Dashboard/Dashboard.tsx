f6aab48022ed26c38e6a872c15532a0db0a6eb32
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-11-02 18:46:22 -0400

    Added feature and table

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,35 @@
+import {
+  Box,
+  Container,
+  Grid,
+  TextField,
+  Button
+} from '@mui/material'
+import {useEffect} from 'react'
+import {LeadsTable} from '../Tables/LeadsTable'
+import { getBusinesses } from '../../features/api/localBusinessSearch/businessScraper'
+
+import example from '../../features/api/localBusinessSearch/example.json'
 
 export const Dashboard = () => {
-  return <h1>Dashboard</h1>
+  console.log('example data', example.data)
+
+  return (
+    <Box p={5}> 
+      <Grid container maxWidth="fluid" spacing={5}>
+        <Grid item xs={4}>
+          <TextField placeholder='Plumbers, Dentists, Roofers..etc' sx={{width:'100%'}} />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField placeholder='San Fransisco, New York, Atlanta...etc' sx={{width:'100%'}} />
+        </Grid>
+        <Grid item xs={4}>
+          <Button sx={{width:'100%', height:'100%'}} variant='contained'>Search</Button>
+        </Grid>
+        <Grid item xs={12}>
+          <LeadsTable data={example.data} />
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
